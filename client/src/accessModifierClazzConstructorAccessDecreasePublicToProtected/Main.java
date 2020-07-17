7ecbbefbcb6b8559a8fc59cd92f4fb6696a5a982
Lina Ochoa <lina.m8a@gmail.com>
2020-07-17 11:44:01 +0200

    Access modifier constructor tests

---
+++ client/src/accessModifierClazzConstructorAccessDecreasePublicToProtected/Main.java
@@ -0,0 +1,20 @@
+package accessModifierClazzConstructorAccessDecreasePublicToProtected;
+
+import testing_lib.accessModifierClazzConstructorAccessDecreasePublicToProtected.AccessModifierClazzConstructorAccessDecreasePublicToProtected;
+
+public class Main extends AccessModifierClazzConstructorAccessDecreasePublicToProtected {
+
+	public Main() {
+		super();
+	}
+	
+	public static void main(String[] args) {
+		
+		int a = 5;
+		
+		new AccessModifierClazzConstructorAccessDecreasePublicToProtected();
+		
+		new Main();
+	}
+	
+}
