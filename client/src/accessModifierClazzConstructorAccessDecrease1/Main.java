6b8b48a867edc1af2b3f58abd1a66b0b1bd44ef1
Thomas Degueule <thomas.degueule@labri.fr>
2021-05-01 15:15:43 +0200

    Split accessModifierClazzConstructorAccessDecrease into atomic cases

--- client/src/accessModifierClazzConstructorAccessDecrease1/Main.java
+++ client/src/accessModifierClazzConstructorAccessDecrease1/Main.java
@@ -1,19 +1,18 @@
-package accessModifierClazzConstructorAccessDecrease;
+package accessModifierClazzConstructorAccessDecrease1;
 
 import testing_lib.accessModifierClazzConstructorAccessDecrease.AccessModifierClazzConstructorAccessDecrease;
 
 public class Main extends AccessModifierClazzConstructorAccessDecrease {
 
-	protected Main(int a, int b, int c) {
-		super(a, b, c);
+	protected Main(int a, int b, int c, int d) {
+		super(a, b, c, d);
 	}
 	
 	public static void main(String[] args) {
 		
 		int a = 5;
 		
-		new Main(a, a, a);
-		
+		new Main(a, a, a, a);
 	}
 	
 }
--- client/src/accessModifierClazzConstructorAccessDecrease2/Main.java
+++ client/src/accessModifierClazzConstructorAccessDecrease2/Main.java
@@ -1,18 +1,14 @@
-package accessModifierClazzConstructorAccessDecrease;
+package accessModifierClazzConstructorAccessDecrease2;
 
 import testing_lib.accessModifierClazzConstructorAccessDecrease.AccessModifierClazzConstructorAccessDecrease;
 
 public class Main extends AccessModifierClazzConstructorAccessDecrease {
 
-	protected Main(int a, int b, int c) {
-		super(a, b, c);
-	}
-	
 	public static void main(String[] args) {
 		
 		int a = 5;
 		
-		new Main(a, a, a);
+		new AccessModifierClazzConstructorAccessDecrease();
 		
 	}
 	
